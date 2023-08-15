88688c73f92b0c81c601d2c827b8c7e56dfff37a
GaryDolan <garydolan4@gmail.com>
2023-08-15 15:44:53 +0100

    d

--- controllers/default.js
+++ controllers/default.js
@@ -18,7 +18,7 @@ function socket() {
         // Spawn terminal
         client.tty = Pty.spawn('python3', ['run.py'], {
             name: 'xterm-color',
-            cols: 110,
+            cols: 90,
             rows: 50,
             cwd: process.env.PWD,
             env: process.env
