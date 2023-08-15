90af6c028e4db7ce26d06b40156880d509037b2d
GaryDolan <garydolan4@gmail.com>
2023-08-15 01:12:53 +0100

    testing fonts and larger display

--- run.py
+++ run.py
@@ -1,12 +1,8 @@
-def display_welcome_msg():
-    pikachu = r"""
+import pyfiglet
 
-  ____       _     __                         ____            _    __       _ _       
- |  _ \ ___ | | __/_/ _ __ ___   ___  _ __   |  _ \ ___  _ __| |_ / _| ___ | (_) ___  
- | |_) / _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \  | |_) / _ \| '__| __| |_ / _ \| | |/ _ \ 
- |  __/ (_) |   <  __/ | | | | | (_) | | | | |  __/ (_) | |  | |_|  _| (_) | | | (_) |
- |_|   \___/|_|\_\___|_| |_| |_|\___/|_| |_| |_|   \___/|_|   \__|_|  \___/|_|_|\___/ 
-                                                                                      
+
+def display_welcome_msg():
+    pikachu = r"""                                                                   
                             ⠸⣷⣦⠤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⠀⠀⠀
                             ⠀⠙⣿⡄⠈⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠔⠊⠉⣿⡿⠁⠀⠀⠀
                             ⠀⠀⠈⠣⡀⠀⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠊⠁⠀⠀⣰⠟⠀⠀⠀⣀⣀
@@ -23,7 +19,28 @@ def display_welcome_msg():
                             ⠀⠀⠀⠈⠻⣿⣦⣈⣧⡀⠀⠀⢸⣿⣿⠀⠀⢀⣼⡀⣨⣿⡿⠁⠀⠀⠀⠀⠀⠀
                             ⠀⠀⠀⠀⠀⠈⠻⠿⠿⠓⠄⠤⠘⠉⠙⠤⢀⠾⠿⣿⠟⠋
 """
-    print(pikachu)
+
+    welcome_msg = pyfiglet.figlet_format("Pokemon-Portfolio", "rectangles")
+    print(welcome_msg)
+    welcome_msg1 = pyfiglet.figlet_format("Pokemon-Portfolio", "small")
+    print(welcome_msg1)
+    welcome_msg2 = pyfiglet.figlet_format("Pokemon-Portfolio", "smslant")
+    print(welcome_msg2)
+    welcome_msg3 = pyfiglet.figlet_format("Pokemon-Portfolio", "big")
+    print(welcome_msg3)
+    welcome_msg4 = pyfiglet.figlet_format("Pokemon-Portfolio", "doom")
+    print(welcome_msg4)
+    welcome_msg5 = pyfiglet.figlet_format("Pokemon-Portfolio", "ogre")
+    print(welcome_msg5)
+    welcome_msg6 = pyfiglet.figlet_format("Pokemon-Portfolio", "rounded")
+    print(welcome_msg6)
+    welcome_msg7 = pyfiglet.figlet_format("Pokemon-Portfolio", "slant")
+    print(welcome_msg7)
+    welcome_msg8 = pyfiglet.figlet_format("Pokemon-Portfolio", "standard")
+    print(welcome_msg8)
+    welcome_msg9 = pyfiglet.figlet_format("Pokemon-Portfolio", "stop")
+    print(welcome_msg9)
+    # print(pikachu)
 
 
 # ----------------------------- MAIN -------------------------------
