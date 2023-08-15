2a36fee793ecad6f699622a5349c61bf35c27432
GaryDolan <garydolan4@gmail.com>
2023-08-15 02:15:41 +0100

    adjust width

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import pyfiglet
+import pyfiglet as pyf
 
 
 def display_welcome_msg():
@@ -20,26 +20,64 @@ def display_welcome_msg():
                             ⠀⠀⠀⠀⠀⠈⠻⠿⠿⠓⠄⠤⠘⠉⠙⠤⢀⠾⠿⣿⠟⠋
 """
 
-    welcome_msg = pyfiglet.figlet_format("Pokemon-Portfolio", "rectangles")
+    font = pyf.Figlet(font="rectangles")
+    welcome_msg = font.renderText("Pokemon Portfolio")
     print(welcome_msg)
-    welcome_msg1 = pyfiglet.figlet_format("Pokemon-Portfolio", "small")
+
+    font1 = pyf.Figlet(font="small")
+    welcome_msg1 = font1.renderText("Pokemon Portfolio")
     print(welcome_msg1)
-    welcome_msg2 = pyfiglet.figlet_format("Pokemon-Portfolio", "smslant")
+
+    font2 = pyf.Figlet(font="smslant")
+    welcome_msg2 = font2.renderText("Pokemon Portfolio")
     print(welcome_msg2)
-    welcome_msg3 = pyfiglet.figlet_format("Pokemon-Portfolio", "big")
+
+    font3 = pyf.Figlet(font="big", width=110)
+    welcome_msg3 = font3.renderText("Pokemon Portfolio")
     print(welcome_msg3)
-    welcome_msg4 = pyfiglet.figlet_format("Pokemon-Portfolio", "doom")
+
+    font4 = pyf.Figlet(font="doom", width=110)
+    welcome_msg4 = font4.renderText("Pokemon Portfolio")
     print(welcome_msg4)
-    welcome_msg5 = pyfiglet.figlet_format("Pokemon-Portfolio", "ogre")
+
+    font5 = pyf.Figlet(font="ogre", width=110)
+    welcome_msg5 = font5.renderText("Pokemon Portfolio")
     print(welcome_msg5)
-    welcome_msg6 = pyfiglet.figlet_format("Pokemon-Portfolio", "rounded")
+
+    font6 = pyf.Figlet(font="rounded", width=110)
+    welcome_msg6 = font6.renderText("Pokemon Portfolio")
     print(welcome_msg6)
-    welcome_msg7 = pyfiglet.figlet_format("Pokemon-Portfolio", "slant")
+
+    font7 = pyf.Figlet(font="slant", width=110)
+    welcome_msg7 = font7.renderText("Pokemon Portfolio")
     print(welcome_msg7)
-    welcome_msg8 = pyfiglet.figlet_format("Pokemon-Portfolio", "standard")
+
+    font8 = pyf.Figlet(font="standard", width=110)
+    welcome_msg8 = font8.renderText("Pokemon Portfolio")
     print(welcome_msg8)
-    welcome_msg9 = pyfiglet.figlet_format("Pokemon-Portfolio", "stop")
+
+    font9 = pyf.Figlet(font="stop", width=110)
+    welcome_msg9 = font9.renderText("Pokemon Portfolio")
     print(welcome_msg9)
+
+    # welcome_msg1 = pyf.figlet_format("Pokemon-Portfolio", "small")
+    # print(welcome_msg1)
+    # welcome_msg2 = pyf.figlet_format("Pokemon-Portfolio", "smslant")
+    # print(welcome_msg2)
+    # welcome_msg3 = pyf.figlet_format("Pokemon-Portfolio", "big")
+    # print(welcome_msg3)
+    # welcome_msg4 = pyf.figlet_format("Pokemon-Portfolio", "doom")
+    # print(welcome_msg4)
+    # welcome_msg5 = pyf.figlet_format("Pokemon-Portfolio", "ogre")
+    # print(welcome_msg5)
+    # welcome_msg6 = pyf.figlet_format("Pokemon-Portfolio", "rounded")
+    # print(welcome_msg6)
+    # welcome_msg7 = pyf.figlet_format("Pokemon-Portfolio", "slant")
+    # print(welcome_msg7)
+    # welcome_msg8 = pyf.fig_format("Pokemon-Portfolio", "standard")
+    # print(welcome_msg8)
+    # welcome_msg9 = pyfig.figlet_format("Pokemon-Portfolio", "stop")
+    # print(welcome_msg9)
     # print(pikachu)
 
 
