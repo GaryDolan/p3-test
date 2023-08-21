5376346cf14c780b82b824c09a64abd0b7e22f64
GaryDolan <garydolan4@gmail.com>
2023-08-21 02:18:14 +0100

    dded

--- run.py
+++ run.py
@@ -13,30 +13,30 @@ import os
 
 # Import the entire gspread library -
 # - access to all classes, methods and functions
-import gspread
+# import gspread
 
 # Import Credentials class from the service account function -
 # - part of the google oauth library
-from google.oauth2.service_account import Credentials
+# from google.oauth2.service_account import Credentials
 
 # Specify what parts of the google account the user has access to
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive",
-]
+# SCOPE = [
+#     "https://www.googleapis.com/auth/spreadsheets",
+#     "https://www.googleapis.com/auth/drive.file",
+#     "https://www.googleapis.com/auth/drive",
+# ]
 
 # Create a Credentials instance from a service account json file
-CREDS = Credentials.from_service_account_file("creds.json")
+# CREDS = Credentials.from_service_account_file("creds.json")
 
 # Create a copy of the credentials with specified scope
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 
 # Create gspread client using gspread authorize method
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
 # Access sheet for project
-SHEET = GSPREAD_CLIENT.open("pokemon_portfolio")
+# SHEET = GSPREAD_CLIENT.open("pokemon_portfolio")
 
 # login = SHEET.worksheet('login')
 
