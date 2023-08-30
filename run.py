77a58340447b2965b646f6223f57d644ace380e2
GaryDolan <garydolan4@gmail.com>
2023-08-30 12:11:12 +0100

    hhh

--- run.py
+++ run.py
@@ -458,7 +458,7 @@ def display_welcome_banner():
     """
     clear_terminal()
 
-    print_art_font("Pokemon Portfolio", "small", "yellow")
+    print_art_font("Pokemon Portfolio", "big", "yellow")
     print()
     print_center_string(
         colored(
